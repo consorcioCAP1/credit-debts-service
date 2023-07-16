35e41b42ab843712dc5cc5be59a19e28fde5e185
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-07-16 17:04:20 -0500

    agregando promer commit de creación de proyecto

---
+++ src/main/java/com/nttdata/bootcamp/creditdebtsservice/CreditDebtsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.creditdebtsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditDebtsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditDebtsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/creditdebtsservice/CreditDebtsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.creditdebtsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditDebtsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
