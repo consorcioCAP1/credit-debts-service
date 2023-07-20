02dd834ad3d89fa3cdd0b29befa57185c9b3689c
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-07-19 19:31:26 -0500

    agregando creditos divididos entre el total de cuotas

--- src/main/java/com/nttdata/bootcamp/creditdebtsservice/service/impl/CreditDebtServiceImpl.java
+++ src/main/java/com/nttdata/bootcamp/creditdebtsservice/service/impl/CreditDebtServiceImpl.java
@@ -6,7 +6,6 @@ import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -19,12 +18,10 @@ import com.nttdata.bootcamp.creditdebtsservice.dto.CreditdebtDto;
 import com.nttdata.bootcamp.creditdebtsservice.repository.CreditDebtRepository;
 import com.nttdata.bootcamp.creditdebtsservice.service.CreditDebtService;
 import com.nttdata.bootcamp.creditdebtsservice.utilities.ConvertJson;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
-@Slf4j
 public class CreditDebtServiceImpl implements CreditDebtService{
 
 	@Autowired
@@ -39,19 +36,19 @@ public class CreditDebtServiceImpl implements CreditDebtService{
 	
 	@Value("${bank-trasaction.url.create}")
     private String bankTransactionkUrlCreate;
-
+	
+	//metodo que registra las deudas de credito en base a la cantidad de cuotas
 	@Override
 	public Flux<CreditDebt> saveCreditDebt(CreditdebtDto creditDebt){
-		//creando variable para trabajar con la fecha inicio de pago
 	    LocalDate paymentStartDate = LocalDate.parse(creditDebt.getPaymentStartDate());
-	    //retornamos el flux 
+	    Double paymentFee = creditDebt.getPaymentAmount()/creditDebt.getNumberBankPaymentInstallments();
 	    return Flux.range(0, creditDebt.getNumberBankPaymentInstallments())
 	            .flatMap(i -> {
 	            	//nos basamos en registrar las cuotas mensuales
 	            	LocalDate paymentDate = paymentStartDate.plusMonths(i);
-	            	//construimos el CreditDebt 
+	            	//construimos el CreditDebt
 	            	CreditDebt creaditDebtDocument = CreditDebt.builder()
-	            			.paymentAmount(creditDebt.getPaymentAmount())
+	            			.paymentAmount(paymentFee)
 							.bankFeeNumber(i+1)
 							.outStandingBankFee(PENDING_PAYMENT_YES)
 							.bankAccountNumber(creditDebt.getBankAccountNumber())
@@ -62,6 +59,7 @@ public class CreditDebtServiceImpl implements CreditDebtService{
 	}
 
 	//realizar pago de una cuota
+	@Override
     public Mono<CreditDebt> updateCreditDebt(CreditdebtDto creditdebtDto) {
         // Buscar el crédito con menor bankFeeNumber que tenga su outStandingBankFee en "YES"
         return repository
